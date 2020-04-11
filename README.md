1129ec326c24941c86a92a6529bee0b43f6ff3b9
Donovan Buck <donovan@donovan.bz>
2020-04-10 23:30:19 -0500

    Version bump

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # huskylens
 Johnny-Five plug in for the HuskyLens AI Vision Sensor
 
-Not ready. Check back soon.
+First draft. Not exactly ready for the world, but you can give it a shot if you're impatient:
+
+````js
+const five = require("johnny-five");
+const Huskylens = require("../index.js")(five);
+
+const board = new five.Board();
+
+board.on("ready", () => {
+
+  const lens = new Huskylens({
+    pins: { rx:11, tx:10 },
+    mode: "OBJECT_RECOGNITION"
+  });
+
+  lens.on("ready", () => {
+    console.log("The Huskylens is connected and ready");
+  });
+
+  lens.on("block", block => {
+    console.log(block);
+  });
+
+});
+````
--- examples/objects.js
+++ examples/objects.js
@@ -1,29 +1,20 @@
-var five = require("johnny-five");
-var Huskylens = require("../index.js")(five);
+const five = require("johnny-five");
+const Huskylens = require("../index.js")(five);
 
 const board = new five.Board();
 
-board.on("ready", function() {
+board.on("ready", () => {
 
   const lens = new Huskylens({
     pins: { rx:11, tx:10 },
-    mode: "LINE_TRACKING"
+    mode: "OBJECT_RECOGNITION"
   });
 
   lens.on("ready", () => {
     console.log("The Huskylens is connected and ready");
   });
 
-  lens.on("okay", () => {
-    console.log("The Huskylens received a Command");
-    console.log("The current mode is " + lens.mode());
-  });
-
   lens.on("block", block => {
-    console.log(block);
-  });
-
-  lens.on("arrow", block => {
     console.log(block);
   });
 
